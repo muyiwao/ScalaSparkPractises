82e98fe0cc06251d8b7229881590ac6959543c44
muyiwao <muyiwa.oladimeji@yahoo.com>
2022-12-18 20:07:36 +0000

    update spark-sql-hive script

--- SparkSqlHiveDemo.scala
+++ SparkSqlHiveDemo.scala
@@ -11,6 +11,9 @@ object SparkSqlHiveDemo extends App {
 
   val spark = SparkSession.builder()
     .config(sparkconf)
+    //.master("local")
+    //.master("local[*]")
+    .enableHiveSupport()
     .getOrCreate()
 
   import spark.implicits._
@@ -23,6 +26,8 @@ object SparkSqlHiveDemo extends App {
     .option("header", "true")
     .load("departuredelays.csv"))
 
+  dataDF.printSchema()
+
   //dataDF.show()
   dataDF.createOrReplaceTempView("us_delay_flights_tbl")
 
@@ -30,19 +35,82 @@ object SparkSqlHiveDemo extends App {
   record_counts.show()
 
   //All flights whose distance is greater than 1,000 miles:
-  val res = spark.sql("""SELECT distance, origin, destination
+  val res = spark.sql("""SELECT date, distance, origin, destination
   FROM us_delay_flights_tbl WHERE distance > 1000
   ORDER BY distance DESC""")
   res.show(10)
 
-  //res.write.option("header",false).csv("output_csv")
-  res.write.format("csv").mode("overwrite").save("flightCsv")
+  // save the output
+  //res.write.format("csv").mode("overwrite").save("flightCsv")
 
 
-  //res.write.mode(SaveMode.Overwrite).csv("csv_output")
+  //Save DataFrame as a new Hive table Use the following code
+  // to save the data frame to a new hive table named flight_table1:
+  // Save df to a new table in Hive
+  // https://kontext.tech/article/294/spark-save-dataframe-to-hive-table
 
+  res.write.mode("overwrite").saveAsTable("flight_table1")
 
-
+  //Show the results using SELECT
+  spark.sql("select * from flight_table1").show(5)
 
 }
+
+/*
+Output:
+Using Spark's default log4j profile: org/apache/spark/log4j-defaults.properties
+root
+ |-- date: string (nullable = true)
+ |-- delay: string (nullable = true)
+ |-- distance: string (nullable = true)
+ |-- origin: string (nullable = true)
+ |-- destination: string (nullable = true)
+
+22/12/18 20:01:19 INFO Persistence: Property hive.metastore.integral.jdo.pushdown unknown - will be ignored
+22/12/18 20:01:19 INFO Persistence: Property datanucleus.cache.level2 unknown - will be ignored
+22/12/18 20:01:20 INFO Datastore: The class "org.apache.hadoop.hive.metastore.model.MFieldSchema" is tagged as "embedded-only" so does not have its own datastore table.
+22/12/18 20:01:20 INFO Datastore: The class "org.apache.hadoop.hive.metastore.model.MOrder" is tagged as "embedded-only" so does not have its own datastore table.
+22/12/18 20:01:20 INFO Datastore: The class "org.apache.hadoop.hive.metastore.model.MFieldSchema" is tagged as "embedded-only" so does not have its own datastore table.
+22/12/18 20:01:20 INFO Datastore: The class "org.apache.hadoop.hive.metastore.model.MOrder" is tagged as "embedded-only" so does not have its own datastore table.
+22/12/18 20:01:20 INFO Query: Reading in results for query "org.datanucleus.store.rdbms.query.SQLQuery@0" since the connection used is closing
+22/12/18 20:01:21 INFO Datastore: The class "org.apache.hadoop.hive.metastore.model.MResourceUri" is tagged as "embedded-only" so does not have its own datastore table.
++--------+
+|count(1)|
++--------+
+| 1391578|
++--------+
+
++--------+--------+------+-----------+
+|    date|distance|origin|destination|
++--------+--------+------+-----------+
+|01091625|    4330|   HNL|        JFK|
+|01051625|    4330|   HNL|        JFK|
+|01081625|    4330|   HNL|        JFK|
+|01021625|    4330|   HNL|        JFK|
+|01041625|    4330|   HNL|        JFK|
+|01061625|    4330|   HNL|        JFK|
+|01071625|    4330|   HNL|        JFK|
+|01011625|    4330|   HNL|        JFK|
+|01111625|    4330|   HNL|        JFK|
+|01031625|    4330|   HNL|        JFK|
++--------+--------+------+-----------+
+only showing top 10 rows
+
+22/12/18 20:01:29 INFO log: Updating table stats fast for flight_table1
+22/12/18 20:01:29 INFO log: Updated size of table flight_table1 to 1374688
++--------+--------+------+-----------+
+|    date|distance|origin|destination|
++--------+--------+------+-----------+
+|01010900|    2151|   JFK|        LAX|
+|01011200|    2151|   JFK|        LAX|
+|01011900|    2151|   JFK|        LAX|
+|01011345|    2151|   JFK|        LAX|
+|01011545|    2151|   JFK|        LAX|
++--------+--------+------+-----------+
+only showing top 5 rows
+
+
+Process finished with exit code 0
+
+ */
 
